c7cf8bad3babbecacb33faf6b60fd766ecf40ae3
Hack Reactor Students <accounts@hackreactor.com>
2017-02-01 17:03:04 -0800

    Implement login-logout-signup functionality with sessions

--- app.js
+++ app.js
@@ -1,115 +1,157 @@
-angular.module('myApp', [
+var app = angular.module('myApp', [
 'ngRoute', 'firebase', 'ngGeolocation', 'ngMap'
-])
-    .config(function ($routeProvider, $httpProvider) {
+]);
 
-        // Initialize Firebase
-        var config = {
-            apiKey: "AIzaSyAhV8qputTImKp3Z5DwwpUmT8lyNRktCfc",
-            authDomain: "palfinder-6d55f.firebaseapp.com",
-            databaseURL: "https://palfinder-6d55f.firebaseio.com",
-            storageBucket: "palfinder-6d55f.appspot.com",
-            messagingSenderId: "673459070337"
-        };
-        firebase.initializeApp(config);
-    })
-    .factory('Auth', function($firebaseAuth){
-        var auth = $firebaseAuth();
+app.config(function ($routeProvider, $httpProvider) {
 
-        return auth;
-    })
-    .controller('controllerOne', function($scope, Auth, $window, $geolocation, NgMap, $firebaseArray){
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyAhV8qputTImKp3Z5DwwpUmT8lyNRktCfc",
+        authDomain: "palfinder-6d55f.firebaseapp.com",
+        databaseURL: "https://palfinder-6d55f.firebaseio.com",
+        storageBucket: "palfinder-6d55f.appspot.com",
+        messagingSenderId: "673459070337"
+    };
+    firebase.initializeApp(config);
+});
 
-        var database = firebase.database();
-        var userRef = database.ref('users');
-        $scope.positions = [];
+app.factory('databaseAndAuth', function($window, $geolocation, NgMap, $firebaseArray){
+    var factory = {};
+    factory.auth = firebase.auth();
+    factory.database = firebase.database();
+    factory.userRef = factory.database.ref('users');
+    return factory;
+});
 
-        //get the coordinates data from the database to populate the map
-        userRef.on("child_added", function(snapshot, prevChildKey) {
-            var position = snapshot.val();
-            $scope.positions.push({userId: position.userId, latitude: position.coordinates.latitude, longitude: position.coordinates.longitude});
-            console.log($scope.positions);
-        });
-        userRef.on("child_changed", function(snapshot) {
-            var position = (snapshot.val());
-            for (var i = 0; i < $scope.positions.length; i ++) {
-              if ($scope.positions[i].userId = $scope.user.userId) {
-                $scope.positions[i].latitude = position.coordinates.latitude;
-                $scope.positions[i].longitude = position.coordinates.longitude;
-                console.log($scope.positions[i]);
-              }
-            }
-        });
-        $scope.register = function (){
-            Auth.$createUserWithEmailAndPassword($scope.username, $scope.password).then(function (user){
-                console.log('success', user);
-            }, function (error){
-                authCtrl.error = error;
+app.controller('registerLogInLogOut', function($scope, databaseAndAuth){
+    $scope.register = function (){
+        var register = databaseAndAuth.auth.createUserWithEmailAndPassword($scope.username, $scope.password);
+        register.catch(function(error) {
+            console.log(error.message);
+        })
+    };
+    $scope.logIn = function (){
+        var login = databaseAndAuth.auth.signInWithEmailAndPassword($scope.username, $scope.password);
+        login.catch(function(error) {
+            console.log(error.message);
+            $scope.badLogin = error.message + ' If you don\'t have an account, please sign up!'
+        })
+    };
+    $scope.logOut = function (){
+        databaseAndAuth.auth.signOut();
+    };
+
+    databaseAndAuth.auth.onAuthStateChanged(function(databaseUser) {
+        if (databaseUser) {
+            console.log(databaseUser.email);
+            $scope.$apply(function() {
+                $scope.loggedIn = true;
             });
-        };
-        $scope.signIn = function () {
-            Auth.$signInWithEmailAndPassword($scope.existingUser, $scope.existingUserPassword).then(function (user){
-                console.log('success', user);
-                $scope.user = {
-                    userId : user.uid
-                }
-            }, function (error){
-                authCtrl.error = error;
-                console.log(error);
-            })
-            //grab the users current location
-            //populate the map
-        }
-
-        $scope.coordinates = {};
-
-
-        $scope.writeUserData = function (userId, coordinates){
-            coordinates = $scope.coordinates;
-            console.log(coordinates);
-            userId = $scope.user.userId;
-            firebase.database().ref('users/' + userId).set({
-                coordinates : coordinates,
-                userId: userId
+        } else {
+            console.log('not logged in');
+            $scope.$apply(function() {
+                $scope.loggedIn = false;
             });
+
         }
-
-        NgMap.getMap().then(function(map) {
-            console.log(map.getCenter().lat());
-            console.log('markers', map.markers);
-            console.log('shapes', map.shapes);
-        });
-
-        //when the device position changes, update the database;
-        $geolocation.watchPosition(function(position) {
-            $scope.coordinates.latitude = position.latitude;
-            $scope.coordinates.longitude = position.longitude;
-
-            $scope.writeUserData(userId, $scope.coordinates);
-        });
-
-        //all users positions need to come from the db
-        //get data from the database
-
-     //   AIzaSyB1hCEtRUrS6jPYMMHeSBoyaZDiTDyouyw
-            //$scope.signInWithGoogle = function () {
-        //    var provider = new firebase.auth.GoogleAuthProvider();
-        //    firebase.auth().signInWithPopup(provider).then(function(result) {
-        //        // This gives you a Google Access Token. You can use it to access the Google API.
-        //        var token = result.credential.accessToken;
-        //        // The signed-in user info.
-        //        var user = result.user;
-        //        console.log("success!!");
-        //        // ...
-        //    }).catch(function(error) {
-        //        // Handle Errors here.
-        //        var errorCode = error.code;
-        //        var errorMessage = error.message;
-        //        // The email of the user's account used.
-        //        var email = error.email;
-        //        // The firebase.auth.AuthCredential type that was used.
-        //        var credential = error.credential;
-        //        // ...
-        //    });
-        //}
     });
+});
+
+// app.controller('logoutCurrentUser', function($scope, databaseAndAuth){
+//     $scope.register = function (){
+//         Auth.createUserWithEmailAndPassword($scope.username, $scope.password).then(function (user){
+//             console.log('success', user);
+//         }, function (error){
+//             authCtrl.error = error;
+//         });
+//     };
+// });
+
+// app.controller('populateMapFromDb', function($scope, databaseAndAuth){
+//     var database = firebase.database();
+//     var userRef = database.ref('users');
+//     $scope.positions = [];
+
+//     //get the coordinates data from the database to populate the map
+//     databaseAndAuth.userRef.on("child_added", function(snapshot, prevChildKey) {
+//         var position = snapshot.val();
+//         $scope.positions.push({userId: position.userId, latitude: position.coordinates.latitude, longitude: position.coordinates.longitude});
+//         console.log($scope.positions);
+//     });
+// });
+
+// app.controller('updateUser', function($scope, databaseAndAuth){
+//     userRef.on("child_changed", function(snapshot) {
+//         var position = (snapshot.val());
+//         for (var i = 0; i < $scope.positions.length; i ++) {
+//           if ($scope.positions[i].userId = $scope.user.userId) {
+//             $scope.positions[i].latitude = position.coordinates.latitude;
+//             $scope.positions[i].longitude = position.coordinates.longitude;
+//             console.log($scope.positions[i]);
+//           }
+//         }
+//     });
+// });
+
+
+// app.controller('logInExistingUser', function($scope, databaseAndAuth){
+//     $scope.logIn = function () {
+//         databaseAndAuth.auth.$signInWithEmailAndPassword($scope.existingUser, $scope.existingUserPassword).then(function (user){
+//             console.log('success', user);
+//             $scope.user = {
+//                 userId : user.uid
+//             }
+//         }, function (error){
+//             authCtrl.error = error;
+//             console.log(error);
+//         })
+//         //grab the users current location
+//         //populate the map
+//     }
+// });
+       
+// app.controller('manuallyAddCoordinates', function($scope, databaseAndAuth){
+//     $scope.coordinates = {};
+//     $scope.writeUserData = function (userId, coordinates){
+//         coordinates = $scope.coordinates;
+//         console.log(coordinates);
+//         userId = $scope.user.userId;
+//         firebase.database().ref('users/' + userId).set({
+//             coordinates : coordinates,
+//             userId: userId
+//         });
+//     }
+// });
+
+app.controller('initializeMap', function($scope, databaseAndAuth, NgMap){
+
+    NgMap.getMap().then(function(map) {
+        console.log(map.setCenter());
+        console.log('markers', map.markers);
+        console.log('shapes', map.shapes);
+    });
+});
+    //when the device position changes, update the database;
+app.controller('watchCurrentUserLocation', function($window, $scope, databaseAndAuth){
+    // $geolocation.watchPosition(function(position) {
+    //     $scope.coordinates.latitude = position.latitude;
+    //     $scope.coordinates.longitude = position.longitude;
+
+    //     $scope.writeUserData(userId, $scope.coordinates);
+    // });
+    var success = function (response) {
+        console.log('success! ', response.coords);
+        $window.coordinates = response.coords;
+    };
+    var error = function (item) {
+        console.log('error! ', item);
+    }
+
+    var options = {
+      enableHighAccuracy: true,
+      timeout: 1,
+      maximumAge: Infinity
+    };
+    navigator.geolocation.watchPosition(success, error, options);
+});
+
