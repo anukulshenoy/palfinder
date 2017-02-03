fb69e139fe5065554b814da1b6028308339e0af2
dsvorc41 <danilosvorcan@live.com>
2017-02-02 16:42:24 -0800

    Implement https server and ssl certificate

--- server/server.js
+++ server/server.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+var fs = require('fs');
+
+var options = {
+  cert: fs.readFileSync('client-cert.pem'),
+  key: fs.readFileSync('client-key.pem')
+};
 
 app.use('/bower_components', express.static(path.join(__dirname, '/../client/bower_components')));
 app.use('/scripts', express.static(path.join(__dirname, '/../client/scripts')));
@@ -8,6 +14,8 @@ app.use('/scripts', express.static(path.join(__dirname, '/../client/scripts')));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '/../client/index.html'));
 })
+
+var server = https.createServer(options, app);
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
